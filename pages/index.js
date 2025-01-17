7f52d8f1eda153ab1c7a093bcf23be560fa35da1
witty code <A0548444334@gmail.com>
2025-01-17 02:37:52 +0200

    Version 0.1.0

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import RouteOptimizer from '../components/RouteOptimizer';
+import RouteOptimizer from '../components/RouteOptimizer'
 
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
       <RouteOptimizer />
     </div>
-  );
+  )
 }
